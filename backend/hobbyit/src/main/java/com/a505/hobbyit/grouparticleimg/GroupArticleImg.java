1486f8f75311a00a7bcd2422097e81d21abf849c
ingkoon <dlswotmd@gmail.com>
2023-01-19 16:41:34 +0900

    feature : Group Article Image 작성

---
+++ backend/hobbyit/src/main/java/com/a505/hobbyit/grouparticleimg/GroupArticleImg.java
@@ -0,0 +1,28 @@
+package com.a505.hobbyit.grouparticleimg;
+
+import com.a505.hobbyit.grouparticle.GroupArticle;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="group_article_img")
+public class GroupArticleImg {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "gai_id", nullable = false)
+    private Long groupArticleImgId;
+
+    @ManyToOne
+    @JoinColumn(name = "group_article_id", nullable = false)
+    private GroupArticle groupArticle;
+
+    @Column(nullable = false, length = 80)
+    private String img;
+
+}
