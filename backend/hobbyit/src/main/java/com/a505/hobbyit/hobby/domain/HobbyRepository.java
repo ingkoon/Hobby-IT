d882406e71521a444b42eb8b29527eb43ac527a5
ingkoon <ing97220@naver.com>
2023-01-24 23:45:38 +0900

    Add: add hobby repostiory

---
+++ backend/hobbyit/src/main/java/com/a505/hobbyit/hobby/domain/HobbyRepository.java
@@ -0,0 +1,27 @@
+package com.a505.hobbyit.hobby.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface HobbyRepository extends JpaRepository<Hobby, Long> {
+
+    @Query("SELECT h "
+            + "FROM Hobby h "
+            + "WHERE h.category in :category ")
+    List<Hobby> findByCategoryAndKeyword(final String category, final String keyword);
+
+
+    @Query(value = "SELECT * "
+            + "FROM HOBBY "
+            + "ORDER BY RAND() "
+            + "LIMIT 10", nativeQuery = true)
+    List<Hobby> findPopularHobby();
+
+    @Query(value = "SELECT * "
+            + "FROM HOBBY "
+            + "ORDER BY CREATED_DATE, RAND() "
+            + "LIMIT 10", nativeQuery = true)
+    List<Hobby> findFreshHobby();
+}
