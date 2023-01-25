1e132d3d295fe97040efad5715e7dbaf5e6e0dbe
ingkoon <dlswotmd@gmail.com>
2023-01-25 14:29:15 +0900

    Add: add Pending Repository

---
+++ backend/hobbyit/src/main/java/com/a505/hobbyit/pending/domain/PendingRepository.java
@@ -0,0 +1,6 @@
+package com.a505.hobbyit.pending.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PendingRepository extends JpaRepository<Pending, Long> {
+}
