9d5a27789b9524308fd79235bb36e840a8c9ab46
ejk9658 <ejk9658@naver.com>
2023-01-26 17:52:38 +0900

    Refactor: refactoring MemberLoginRequest

---
+++ backend/hobbyit/src/main/java/com/a505/hobbyit/member/dto/MemberLoginRequest.java
@@ -0,0 +1,33 @@
+package com.a505.hobbyit.member.dto;
+
+import com.a505.hobbyit.member.domain.Member;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class MemberLoginRequest {
+
+    @NotNull(message = "이메일은 필수 입력값입니다.")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
+    private String email;
+
+    @NotNull(message = "비밀번호는 필수 입력값입니다.")
+    private String password;
+
+    public UsernamePasswordAuthenticationToken toAuthentication() {
+        return new UsernamePasswordAuthenticationToken(email, password);
+    }
+
+    public Member toEntity() {
+        return Member.builder().email(email)
+                .password(password)
+                .build();
+    }
+}
