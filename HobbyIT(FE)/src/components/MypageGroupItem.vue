0eda41441164ff048385c9cbdf653a10272ec8a2
subinKim <subin3277@gmail.com>
2023-01-31 13:19:18 +0900

    feature:add group

--- HobbyIT(FE)/src/components/MypageGroupItem.vue
+++ HobbyIT(FE)/src/components/MypageGroupItem.vue
@@ -1,16 +1,19 @@
 <template>
   <v-card
     class="mx-auto"
-    width="180"
+    width="250px"
     style="background-color:#ffffff00; padding:10px"
     >
-    <v-img
-      class="align-end text-white"
-      height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-      cover
-    >
-    </v-img>
+    <div style="display:flex; position: relative;">
+      <v-img
+        class="align-end text-white"
+        height="250"
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        cover
+      >
+      </v-img>
+      <div id="tri1"></div>
+    </div>
 
     <v-card-subtitle class="pt-4" style="color:#ffffff; opacity:1">
       Sk8ter Boy
@@ -19,6 +22,8 @@
     <v-card-text style="display:flex; justify-content:space-between; color:#b3b3b3;">
       
     </v-card-text>
+
+    
   </v-card>
 </template>
 
@@ -29,5 +34,17 @@ export default {
 </script>
 
 <style>
+img {
+  border : 2px solid white;
+}
 
+#tri1 {
+  width: 0;
+  height: 0;
+  border-bottom: 20px solid transparent;
+  border-top: 20px solid #FA8EB670;
+  border-left: 20px solid #FA8EB670;
+  border-right: 20px solid transparent;
+  align-self: flex-end;
+}
 </style>
