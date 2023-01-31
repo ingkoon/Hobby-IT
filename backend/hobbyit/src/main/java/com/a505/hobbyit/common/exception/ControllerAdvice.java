a2ba6867bf7c85bb0aab319ed4314c413de5ae41
ingkoon <dlswotmd@gmail.com>
2023-01-31 17:32:58 +0900

    Add: Add DuplicatedHobbyException to ControllerAdvice.java

--- backend/hobbyit/src/main/java/com/a505/hobbyit/common/exception/ControllerAdvice.java
+++ backend/hobbyit/src/main/java/com/a505/hobbyit/common/exception/ControllerAdvice.java
@@ -1,7 +1,9 @@
 package com.a505.hobbyit.common.exception;
 
 import com.a505.hobbyit.common.exception.dto.ErrorResponse;
+import com.a505.hobbyit.hobby.exception.DuplicatedHobbyException;
 import com.a505.hobbyit.hobby.exception.InvalidHobbyException;
+import com.a505.hobbyit.hobbymember.exception.UnAuthorizedHobbyException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -16,9 +18,23 @@ public class ControllerAdvice {
     private static final String INVALID_DTO_FIELD_ERROR_MESSAGE_FORMAT = "%s 필드는 %s (전달된 값: %s)";
 
     @ExceptionHandler({InvalidHobbyException.class})
-    public ResponseEntity<ErrorResponse> handleInvalidHobbyException(final RuntimeException e){
+    public ResponseEntity<ErrorResponse> handleInvalidException(final RuntimeException e){
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler({DuplicatedHobbyException.class})
+    public ResponseEntity<ErrorResponse> handleDuplicatedException(final RuntimeException e){
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+    }
+
+    @ExceptionHandler({UnAuthorizedHobbyException.class})
+    public  ResponseEntity<ErrorResponse> handleUnAuthorizedException(final RuntimeException e){
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 }
