62685e8f5e076c5df5b98206496b58094fe345a7
ejk9658 <ejk9658@naver.com>
2023-02-01 10:48:09 +0900

    Add : Add permitAll swagger

--- backend/hobbyit/src/main/java/com/a505/hobbyit/security/WebSecurityConfig.java
+++ backend/hobbyit/src/main/java/com/a505/hobbyit/security/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -20,20 +21,21 @@ public class WebSecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final StringRedisTemplate stringRedisTemplate;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .httpBasic().disable()
-            .csrf().disable()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authorizeHttpRequests()
-                .requestMatchers("/**", "/swagger-ui.html", "/swagger-ui/**", "/api/**", "/api/v1/users/sign-up", "/api/v1/users/login", "/api/v1/users/authority", "/api/v1/users/reissue", "/api/v1/users/logout").permitAll()
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/**", "/swagger-ui/**").permitAll()
                 .requestMatchers("/api/v1/users/userTest").hasRole("USER")
-            .requestMatchers("/api/v1/users/adminTest").hasRole("ADMIN")
-            .and()
-            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, stringRedisTemplate), UsernamePasswordAuthenticationFilter.class);
-            // JwtAuthenticationFilter를 UsernamePasswordAuthentictaionFilter 전에 적용시킨다.
+                .requestMatchers("/api/v1/users/adminTest").hasRole("ADMIN")
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, stringRedisTemplate), UsernamePasswordAuthenticationFilter.class);
+        // JwtAuthenticationFilter를 UsernamePasswordAuthentictaionFilter 전에 적용시킨다.
         return http.build();
     }
 
