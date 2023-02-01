db8a653c3fc17839f97d66fe0a3211fbccac5988
ingkoon <dlswotmd@gmail.com>
2023-02-01 17:50:18 +0900

    Add: add HobbyArticleLikes repository

---
+++ backend/hobbyit/src/main/java/com/a505/hobbyit/hobbyarticlelikes/domain/HobbyArticleLikesRepository.java
@@ -0,0 +1,6 @@
+package com.a505.hobbyit.hobbyarticlelikes.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HobbyArticleLikesRepository extends JpaRepository<HobbyArticleLikes, Long> {
+}
