bd98f56edff656cf9dabfd6306603f840b7482bf
ingkoon <dlswotmd@gmail.com>
2023-02-01 17:48:55 +0900

    Add: add Member dto

--- backend/hobbyit/src/main/java/com/a505/hobbyit/hobbymember/dto/HobbyMemberUpdateRequest.java
+++ backend/hobbyit/src/main/java/com/a505/hobbyit/hobbymember/dto/HobbyMemberUpdateRequest.java
@@ -1,8 +1,12 @@
 package com.a505.hobbyit.hobbymember.dto;
 
 import com.a505.hobbyit.hobbymember.enums.HobbyMemberPrivilege;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class HobbyMemberUpdateRequest {
     HobbyMemberPrivilege privilege;
