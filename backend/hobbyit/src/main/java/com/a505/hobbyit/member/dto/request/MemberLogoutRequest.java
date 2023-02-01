7f18dc5bb8338f2d875ef4ebfc368b7c02f0468b
ejk9658 <ejk9658@naver.com>
2023-02-01 17:51:43 +0900

    Add: Add annotation NoArgsConstructor

--- backend/hobbyit/src/main/java/com/a505/hobbyit/member/dto/request/MemberLogoutRequest.java
+++ backend/hobbyit/src/main/java/com/a505/hobbyit/member/dto/request/MemberLogoutRequest.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class MemberLogoutRequest {
--- backend/hobbyit/src/main/java/com/a505/hobbyit/member/dto/request/MemberSignupRequest.java
+++ backend/hobbyit/src/main/java/com/a505/hobbyit/member/dto/request/MemberSignupRequest.java
@@ -5,7 +5,9 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class MemberSignupRequest {
