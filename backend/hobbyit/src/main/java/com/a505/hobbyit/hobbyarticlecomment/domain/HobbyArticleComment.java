fc5001d4dbadca97214f3c3f1bdffb28cc120c93
ingkoon <dlswotmd@gmail.com>
2023-02-01 17:46:43 +0900

    Add: add HobbyArticle comment

--- backend/hobbyit/src/main/java/com/a505/hobbyit/hobbyarticlecomment/domain/HobbyArticleComment.java
+++ backend/hobbyit/src/main/java/com/a505/hobbyit/hobbyarticlecomment/domain/HobbyArticleComment.java
@@ -1,39 +1,30 @@
 package com.a505.hobbyit.hobbyarticlecomment.domain;
 
-
 import com.a505.hobbyit.common.BaseEntity;
 import com.a505.hobbyit.hobbyarticle.domain.HobbyArticle;
 import com.a505.hobbyit.member.domain.Member;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Getter
+@AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Table(name = "hobby_article_comment")
 public class HobbyArticleComment extends BaseEntity {
-
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name="hobby_art_id", nullable = false)
+    @JoinColumn(name = "hobby_art_id", nullable = false)
     private HobbyArticle hobbyArticle;
 
     @ManyToOne
-    @JoinColumn(name="mem_id", nullable = false)
+    @JoinColumn(name = "mem_id", nullable = false)
     private Member member;
 
     @Column(nullable = false, length = 255)
     private String content;
-
-    @Builder
-    public HobbyArticleComment(HobbyArticle hobbyArticle, Member member, String content) {
-        this.hobbyArticle = hobbyArticle;
-        this.member = member;
-        this.content = content;
-    }
 }
