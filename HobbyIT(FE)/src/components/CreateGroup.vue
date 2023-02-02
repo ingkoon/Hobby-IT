c0a255ca8bf17ef89b390f24a9db54c28ed4d5f2
서철원 <96charles@naver.com>
2023-02-02 14:37:10 +0900

    Add: Home CSS & CreateGroup

--- HobbyIT(FE)/src/components/CreateGroup.vue
+++ HobbyIT(FE)/src/components/CreateGroup.vue
@@ -1,17 +1,17 @@
 <template>
-  <div style="align-self:center;">
-    <div style="width:1200px; border-radius: 20px; background-color:#20225A; padding:1px;">
+  <div class="bg" style="align-self:center;">
+    <div style="width:1000px; border-radius: 20px; background-color:#20225A; padding:1px;">
       <v-card id="card" style="margin: 12px;">
         <div id="notopa" style="color:white; height:60px; display:flex; justify-content:space-between; padding:10px 5px; background: linear-gradient(180deg, rgba(14, 15, 40, 0) 0%, #0E0F28 100%);">
           <span></span>
-          <span>HOBBY 만들기</span>
-          <v-icon icon="mdi-close" size="small" @click="closecreategroup"></v-icon>
+          <span style="font-size: 20px; font-weight: 700; margin-left: 80px;">HOBBY 만들기</span>
+          <v-icon icon="mdi-close" size="24" @click="closecreategroup" style="margin-right: 10px;"></v-icon>
         </div>
         <div style="display:flex; background-color:#0E0F28;">
-          <div style="width:490px; height:700px; background-color:#0E0F28; display: flex; align-items: center; justify-content: center;
+          <div style="width:350px; height:500px; background-color:#0E0F28; display: flex; align-items: center; justify-content: center;
                       border: 4px solid #FA8EB6;" @click="uploadimg">
             <v-icon icon = "mdi-plus-circle-outline" style="align-self: center; color: white; z-index:1;" v-if="!isImage"></v-icon>
-            <img style="width:490px; height:700px; object-fit: cover;" id="realimg" v-else>
+            <img style="width:350px; height:500px; object-fit: cover;" id="realimg" v-else>
           </div>
           <input type="file" id="uploadimg" accept="image/*" required style="display:none">
           <div id="tri1" style=""></div>
@@ -83,6 +83,16 @@ export default {
 </script>
 
 <style scoped>
+.bg{
+  font-family: 'LINESeedKR-Rg';
+}
+@font-face {
+    font-family: 'LINESeedKR-Rg';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Rg.woff2') format('woff2');
+    font-weight: 400;
+    font-style: normal;
+}
+
 #groupname {
   font-size:20px;
   background: linear-gradient(to bottom, #642EFE, #FA8EB6);
--- HobbyIT(FE)/src/components/CreateSteps.vue
+++ HobbyIT(FE)/src/components/CreateSteps.vue
@@ -1,28 +1,28 @@
 <template>
   <v-card
-    class="mx-auto"
-    max-width="600"
-    style="height:700px; background-color:#0E0F28; color: white;"
+    class="bg mx-auto"
+    width="600"
+    style="height:500px; background-color:#0E0F28; color: white;"
   >
     <!-- 숫자 -->
-        <v-card-title class="text-h6 font-weight-regular justify-space-between" style="display: flex; align-items: center;">
+        <v-card-title class="justify-space-between" style="display: flex; align-items: center; text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">
           <v-avatar
-            size="64"
+            size="52"
             style="border-width: 2px; border-style: solid; border-color: white; color: white; margin-left:10px;"
             id="s1"
-          >1</v-avatar>
+          ><v-icon icon="mdi-numeric-1" size="36"></v-icon></v-avatar>
           <hr style="color:white; width:150px;">
           <v-avatar
-            size="64"
+            size="52"
             style="border-width: 2px; border-style: solid; border-color: white; color: white;"
             id="s2"
-          >2</v-avatar>
+          ><v-icon icon="mdi-numeric-2" size="36"></v-icon></v-avatar>
           <hr style="color:white; width:150px;">
           <v-avatar
-            size="64"
+            size="52"
             style="border-width: 2px; border-style: solid; border-color: white; color: white; margin-right: 20px;"
             id="s3"
-          >3</v-avatar>
+          ><v-icon icon="mdi-numeric-3" size="36"></v-icon></v-avatar>
         </v-card-title>
 
     <v-window v-model="step">
@@ -31,39 +31,39 @@
 
         <!-- 양식 -->
         <v-card-text>
-          <h2 style="font-size:20px; text-align:center; margin: 5px;">먼저 모임의 썸네일, 이름, 카테고리를 정해볼까요?</h2>
-          <div style="display: flex; align-items:center; margin: 20px 0px 10px 0px;">
+          <h2 style="font-size:24px; text-align:center; margin: 5px;">먼저 모임의 썸네일, 이름, 카테고리를 정해볼까요?</h2>
+          <div style="display: flex; align-items:center; margin: 30px 0px 10px 0px;">
             <v-icon icon="mdi-at" size="36" color="blue-lighten-2" style="align-self: center;"></v-icon>
-            <span style="font-size:26px; font-weight: 700; margin-left: 10px;">HOBBY 이름</span>
-            <span style="font-size:12px; margin-left: 80px;">!HOBBY이름과 카테고리는 추후 변경이 불가합니다.</span>
+            <span style="font-size:20px; font-weight: 700; margin-left: 10px;">HOBBY 이름</span>
+            <span style="font-size:14px; margin-left: 80px;">! HOBBY이름과 카테고리는 추후 변경이 불가합니다.</span>
           </div>
 
           <input
           type="text"
           v-model="hobbytitle"
           placeholder="Hobby 이름"
-          style="border:2px solid #24B1D0; border-radius:10px; height:60px; width:550px; padding-left:20px; margin:15px 0px 7px;
-          color:white; font-size: 26px; font-weight: 700;"
+          style="border:2px solid #24B1D0; border-radius:10px; height:40px; width:500px; padding-left:20px; margin:0px 0px 7px 20px;
+          color:white; font-size: 16px; font-weight: 700;"
           >
-          <div style="display: flex; align-items:center; margin: 20px 0px 10px 0px;">
+          <div style="display: flex; align-items:center; margin: 20px 0px 0px 0px;">
             <v-icon icon="mdi-bookmark-outline" size="36" color="blue-lighten-2" style="align-self: center;"></v-icon>
-            <span style="font-size:26px; font-weight: 700; margin-left: 10px;">카테고리</span>
+            <span style="font-size:20px; font-weight: 700; margin-left: 10px;">카테고리</span>
           </div>
           <div style="display: flex; align-items: center; text-align: center;">
-            <v-item-group mandatory selected-class="bg-purple" v-model="myValue">
+            <v-item-group mandatory="force" selected-class="bg-pink" v-model="myValue">
               <v-item
                 v-for="n in 12"
                 :key="n"
                 v-slot="{ selectedClass, toggle }"
               >
-                <v-chip
+                <v-btn
                   :class="selectedClass"
-                  style="width:120px; height:60px; justify-content:center; text-align: center; margin: 10px; border: 4px solid #FA8EB6;
-                   background-color: white; color: black; font-size: 20px; font-weight: 700; text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);"
+                  style="width:70px; height:45px; justify-content:center; text-align: center; margin: 10px; border: 2px solid #FA8EB6;
+                  border-radius: 50px; background-color: #0E0F28; color: white; font-size: 13px; font-weight: 700; text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);"
                   @click="toggle"
                 >
-                  {{ tags[n-1] }}
-                </v-chip>
+                  #{{ tags[n-1] }}
+                </v-btn>
               </v-item>
             </v-item-group>
           </div>
@@ -81,7 +81,7 @@
             <v-icon icon="mdi-message-question-outline" size="36" color="blue-lighten-2" style="align-self: center;"></v-icon>
             <span style="font-size:20px; font-weight: 700; margin-left: 10px;">HOBBY 소개</span>
           </div>
-          <v-textarea label="intro" variant="outlined"></v-textarea>
+          <v-textarea label="어떤 모임인가요" variant="outlined"></v-textarea>
         </v-card-text>
       </v-window-item>
 
@@ -89,25 +89,108 @@
       <v-window-item :value="3">
         <!-- 양식 -->
         <v-card-text>
-          <div style="display: flex; align-items:center; margin: 20px 0px 10px 0px;">
-            <v-icon icon="mdi-account-multiple-outline" size="36" color="blue-lighten-2" style="align-self: center;"></v-icon>
-            <span style="font-size:26px; font-weight: 700; margin-left: 10px;">최대 활동 인원</span>
-            <span style="font-size:12px; margin-left: 80px;">!활동인원과 가입조건은 추후 변경이 불가합니다</span>
+          <div>
+            <h2 style="font-size:24px; text-align:center; margin: 5px 0px -20px 0px;">거의 끝났습니다 :)</h2>
           </div>
-          <div style="display: flex; align-items:center; margin: 20px 0px 10px 0px;">
-            <v-icon icon="mdi-login" size="36" color="blue-lighten-2" style="align-self: center;"></v-icon>
-            <span style="font-size:20px; font-weight: 700; margin-left: 10px;">가입 조건</span>
+          <div style="display: flex; align-items:center; margin: 40px 0px 0px 0px;">
+            <v-icon icon="mdi-account-multiple-outline" size="30" color="blue-lighten-2" style="align-self: center;"></v-icon>
+            <span style="font-size:18px; font-weight: 700; margin-left: 10px;">최대 활동 인원</span>
+            <span style="font-size:12px; margin-left: 150px;">! 활동인원과 가입조건은 추후 변경이 불가합니다</span>
           </div>
-          <v-textarea label="intro" variant="outlined"></v-textarea>
+          <v-item-group mandatory="force" selected-class="bg-pink" v-model="myValue" style="display:flex;">
+              <v-item
+                v-for="n in 5"
+                :key="n"
+                v-slot="{ selectedClass, toggle }"
+              >
+              <div style="display:flex; flex-direction: column; align-items:center; margin: 0px 30px 0px -2px;">
+                <v-btn
+                  :class="selectedClass"
+                  size="60"
+                  style="justify-content:center; text-align: center; margin: 15px; border: 2px solid white;
+                  border-radius: 50px; background-color: #0E0F28; color: white; font-size: 20px; font-weight: 700; text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);"
+                  @click="toggle"
+                >
+                  {{ people[n-1] }}
+                </v-btn>
+                <div style="width:55px; height:20px; border:2px solid white; border-radius: 40px 40px 0px 0px / 17px 17px 0px 0px; margin-top:-10px;"></div>
+              </div>
+              </v-item>
+            </v-item-group>
+          <div style="display: flex; align-items:center; margin: 20px 0px 5px 0px;">
+            <v-icon icon="mdi-login" size="30" color="blue-lighten-2" style="align-self: center;"></v-icon>
+            <span style="font-size:18px; font-weight: 700; margin-left: 10px;">가입 유형</span>
+          </div>
+          <v-item-group mandatory="force" style="margin-left: 35px;">
+            <v-container>
+              <v-row>
+                <v-item v-slot="{ isSelected, toggle }">
+                  <v-card
+                    :color="isSelected ? 'cyan' : ''"
+                    class="d-flex align-center"
+
+                    width="250"
+                    height="100"
+                    @click="toggle"
+                    style="background-color: #0E0F28; border: solid #24B1D0; border-width: 2px 1px 2px 2px; border-radius: 10px 0px 0px 10px;"
+                  >
+                    <div
+                      class="flex-grow-1 text-center"
+                      style="color: white; font-size: 24px; font-weight: 700; display: flex; flex-direction: column; align-items: center;"
+                      v-if="isSelected"
+                    >
+                    <v-icon icon="mdi-lock-open-variant-outline" size="16" style="margin: -10px 0px 10px 0px;"></v-icon>
+                      자유 가입
+                    </div>
+                    <div
+                      class="flex-grow-1 text-center"
+                      style="color: white; font-size:20px; font-weight: 700;"
+                      v-if="!isSelected"
+                    >
+                    자유 가입<br><v-divider style="margin:5px;"></v-divider><span style="font-size:12px; font-weight: 400;">관리자의 별도 승인 없이<br>바로 가입 및 활동이 가능합니다.</span>
+                    </div>
+                  </v-card>
+                </v-item>
+                <v-item v-slot="{ isSelected, toggle }">
+                  <v-card
+                    :color="isSelected ? 'pink' : ''"
+                    class="d-flex align-center"
+                    dark
+                    width="250"
+                    height="100"
+                    @click="toggle"
+                    style="background-color: #0E0F28; border: solid #24B1D0; border-width: 2px 2px 2px 1px; border-radius: 0px 10px 10px 0px;"
+                  >
+                    <div
+                      class="flex-grow-1 text-center"
+                      style="color: white; font-size: 24px; font-weight: 700; display: flex; flex-direction: column; align-items: center;"
+                      v-if="isSelected"
+                    >
+                    <v-icon icon="mdi-lock-outline" size="16" style="margin: -10px 0px 10px 0px;"></v-icon>
+                    신청 가입
+                    </div>
+                    <div
+                      class="flex-grow-1 text-center"
+                      style="color: white; font-size:20px; font-weight:700;"
+                      v-if="!isSelected"
+                    >
+                     신청 가입<br><v-divider style="margin:5px;"></v-divider><span style="font-size:12px; font-weight:400;">관리자의 가입승인이 있어야<br>활동이 가능합니다.</span>
+                    </div>
+                  </v-card>
+                </v-item>
+              </v-row>
+            </v-container>
+          </v-item-group>
         </v-card-text>
       </v-window-item>
     </v-window>
 
-    <v-divider></v-divider>
+    <v-divider style="margin-top:-10px;"></v-divider>
 
     <v-card-actions>
       <v-btn
         v-if="step > 1"
+        size="48"
         color="white"
         @click="beforestep"
         style="background-color: #567CFF;"
@@ -117,6 +200,7 @@
       <v-spacer></v-spacer>
       <v-btn
         v-if="step < 3"
+        size="48"
         color="white"
         @click="nextstep"
         :disabled="!isButtonAbled"
@@ -127,9 +211,10 @@
 
       <v-btn
         v-if="step == 3"
+        size="48"
         color="white"
         style="background-color: #FA8EB6;"
-        icon="mdi-check-bold"
+        icon="mdi-check"
       >
       </v-btn>
     </v-card-actions>
@@ -140,7 +225,8 @@
   export default {
       data: () => ({
         step: 1,
-        tags: ["#게임", "#스터디", "#공예", "#코딩", "#스포츠", "#문화", "#예술", "#음악", "#패션", "#여행", "#요리", "#파티"],
+        tags: ["게임", "스터디", "공예", "코딩", "스포츠", "문화", "예술", "음악", "패션", "여행", "요리", "파티"],
+        people: [12, 13, 14, 15, 16],
         hobbytitle : '',
         myValue: undefined,
       }),
@@ -170,11 +256,11 @@
           const s3 = document.getElementById("s3")
           this.step--
           if(this.step == 1){
-            s1.setAttribute("style","background-color: #FA8EB6")
-            s2.setAttribute("style","background-color: #567CFF")
+            s1.setAttribute("style","background-color: #FA8EB6; font-weight: 700;")
+            s2.setAttribute("style","background-color: #567CFF; font-weight: 400;")
           }else if(this.step == 2){
-            s2.setAttribute("style","background-color: #FA8EB6")
-            s3.setAttribute("style","background-color: #567CFF")
+            s2.setAttribute("style","background-color: #FA8EB6; font-weight: 700;")
+            s3.setAttribute("style","background-color: #567CFF; font-weight: 400;")
           }
         },
         nextstep(){
@@ -184,29 +270,28 @@
           this.step++
           if(this.step == 2){
             console.log("dd")
-            s1.setAttribute("style","background-color:#567CFF;")
-            s2.setAttribute("style","background-color: #FA8EB6")
+            s1.setAttribute("style","background-color: #567CFF; font-weight: 400;")
+            s2.setAttribute("style","background-color: #FA8EB6; font-weight: 700;")
           }else if(this.step == 3){
-            s2.setAttribute("style","background-color: #567CFF")
-            s3.setAttribute("style","background-color: #FA8EB6")
+            s2.setAttribute("style","background-color: #567CFF; font-weight: 400;")
+            s3.setAttribute("style","background-color: #FA8EB6; font-weight: 700;")
           }
         },
 
       },
-
-      // mounted(){
-      //   console.log("init")
-      //   const s1 = document.getElementById("s1")
-      //   const s2 = document.getElementById("s2")
-      //   const s3 = document.getElementById("s3")
-      //   s1.setAttribute("style","background-color:#FA8EB6;")
-      //   s2.setAttribute("style","background-color:#567CFF")
-      //   s3.setAttribute("style","background-color:#567CFF;")
-      // }
     }
 </script>
 
 <style>
+.bg{
+  font-family: 'LINESeedKR-Rg';
+}
+@font-face {
+    font-family: 'LINESeedKR-Rg';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Rg.woff2') format('woff2');
+    font-weight: 400;
+    font-style: normal;
+}
 #s1 {
   background-color : #FA8EB6
 }
