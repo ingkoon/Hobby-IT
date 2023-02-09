b101d63994fe16c00a0c598514beb054fe07e32d
ingkoon <dlswotmd@gmail.com>
2023-02-09 17:18:46 +0900

    Add: add ErrorModel/ErrorCode

--- backend/src/main/java/com/a505/hobbyit/common/exception/dto/ErrorResponse.java
+++ backend/src/main/java/com/a505/hobbyit/common/exception/dto/ErrorResponse.java
@@ -1,13 +1,30 @@
 package com.a505.hobbyit.common.exception.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class ErrorResponse {
-    private final String message;
+    private String code;
+    private String message;
+    private int status;
+    private String detail;
 
     public ErrorResponse(final String message) {
         this.message = message;
     }
-
+    public ErrorResponse(ErrorCode code) {
+        this.code = code.getCode();
+        this.status = code.getStatus();
+        this.message = code.getMessage();
+        this.detail = code.getDetail();
+    }
     public String getMessage() {
         return message;
+    }
+
+    public static ErrorResponse of(ErrorCode code){
+        return new ErrorResponse(code);
     }
 }
