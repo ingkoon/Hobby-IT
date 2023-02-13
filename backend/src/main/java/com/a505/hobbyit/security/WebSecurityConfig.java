3b37aab7cda22865fd68a007156b9a83a5d763c9
ingkoon <dlswotmd@gmail.com>
2023-02-13 16:12:12 +0900

    Add: add kakako oauth api url to web config

--- backend/src/main/java/com/a505/hobbyit/security/WebSecurityConfig.java
+++ backend/src/main/java/com/a505/hobbyit/security/WebSecurityConfig.java
@@ -32,6 +32,7 @@ public class WebSecurityConfig {
                 .and()
                 .authorizeHttpRequests()
                 .requestMatchers("/api/**", "/swagger-ui/**", "/**").permitAll()
+                .requestMatchers("https://kauth.kakao.com/oauth/**").permitAll()
                 .requestMatchers("/api/v1/users/userTest").hasRole("USER")
                 .requestMatchers("/api/v1/users/adminTest").hasRole("ADMIN")
                 .and()
