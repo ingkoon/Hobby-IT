8cd6965470c2c5dfc4edc291be47840a822bdb9a
ejk9658 <ejk9658@naver.com>
2023-02-15 22:02:45 +0900

    Add: add page 404

--- HobbyIT(FE)/src/views/NotFound.vue
+++ HobbyIT(FE)/src/views/NotFound.vue
@@ -1,9 +1,23 @@
 <template>
-  <h3>페이지를 찾을 수 없습니다</h3>
+  <div>
+    <h3>페이지를 찾을 수 없습니다 !!!!</h3>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+div {
+  height: 100vh;
+  background: url(assets/gif/SPACEMAN.gif) no-repeat center;
+  background-size: cover;
+  position: relative;
+}
+h3 {
+  color: white;
+  text-align: center;
+  margin-top: 2em;
+}
+</style>
