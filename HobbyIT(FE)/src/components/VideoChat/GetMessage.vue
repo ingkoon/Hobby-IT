83e7905230d7afec342b240f5684332bd9233d21
ejk9658 <ejk9658@naver.com>
2023-02-17 02:33:34 +0900

    Update: update css of chat message

--- HobbyIT(FE)/src/components/VideoChat/GetMessage.vue
+++ HobbyIT(FE)/src/components/VideoChat/GetMessage.vue
@@ -1,8 +1,8 @@
 <template>
-  <div style="position:relative; display:flex; margin:10px;">
-    <div id="lefttri" :style="`border-left: 15px solid ${color}; border-bottom: 15px solid ${color}; `"></div>
-    <div id="text" :style="`background-color: ${color}`" style="padding: auto 10px; vertical-align: middle;">
-      <div style="font-family: linefont;">{{ msg.from }}</div>
+  <div style='position:relative; display:flex; margin:10px;'>
+    <div id='lefttri' :style='`border-right: 15px solid ${color}; border-bottom: 15px solid ${color}; `'></div>
+    <div id='text' :style='`background-color: ${color}`'>
+      <div style='font-family: linefont;'>{{ msg.from }}</div>
       {{ msg.data }}
     </div>
   </div>
@@ -21,21 +21,21 @@ export default {
   #lefttri {
     position: absolute;
     bottom: 0;
-    left: 75%;
     width: 0;
     height: 0;
     border-top : 15px solid transparent;
-    border-right : 15px solid transparent;
+    border-left: 15px solid transparent;
   }
 
   #text {
     background-color: white;
-    border-radius: 5px 5px 0 5px;
+    border-radius: 5px 5px 5px 0;
     text-overflow: ellipsis;
     width: 70%;
     font-size : 20px;
     margin-left: 28px;
-    padding : 5px;
+    padding : 8px;
     word-break: break-all;
+    vertical-align: middle;
   }
 </style>
