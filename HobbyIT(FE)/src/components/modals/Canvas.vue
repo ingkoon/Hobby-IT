1b1e8323317977d693b527e5a42a3dd9474b0d25
subinKim <subin3277@gmail.com>
2023-02-16 16:42:27 +0900

    Add :get canvas

--- HobbyIT(FE)/src/components/modals/Canvas.vue
+++ HobbyIT(FE)/src/components/modals/Canvas.vue
@@ -2,13 +2,30 @@
 
 <template>
   <div style="align-self: center">
-    <v-card style="width: 800px; height: 530px; background-color: #0e0f28">
+    <v-card style="width: 800px; height: 600px; background-color: #0e0f28">
       <div style="color: white; display: flex; justify-content: space-between; margin: 10px 5px">
-        <span id="groupname">To. John, 나 여행가고싶어</span>
-        <span>방명록 작성</span>
+        <span id="groupname">To. {{ this.data[2] }}</span>
+        <span>방명록</span>
         <v-icon icon="mdi-close" size="small" @click="closemodal"></v-icon>
       </div>
-    
+      
+      <v-row style="width:100%; height: 90%; margin : auto; overflow-y:auto ;">
+        <v-col
+          v-for="n in canvaslist.length"
+          :key="n"
+          class="d-flex child-flex"
+          cols="4"
+          style=""
+        >
+          <v-img
+            :src="canvaslist[n]"
+            aspect-ratio="1"
+            cover
+          >
+          </v-img>
+        </v-col>
+      </v-row>
+
     </v-card>
   </div>
 </template>
@@ -19,17 +36,11 @@ import { getGroupVisitorBook } from '@/api/hobby';
 export default {
   data() {
     return {
-      ctx: '',
-      canvas: '',
-      date: '',
-      senddate : '',
-      background: '',
-      bglist: ['#F7FDB0', '#ABFAB3', '#BFE0FE', '#FECDCD'],
-      bglist2: ['#D3D98F', '#63D382', '#AC94F1', '#DAA6A6'],
+      canvaslist : [],
     };
   },
   props : {
-    data : Object //[date, groupid]
+    data : Object //[date, groupid, groupname]
   },
   mounted() {
     this.getcanvas(this.data[1], this.data[0])
@@ -56,6 +67,9 @@ export default {
       try {
         const { data } = await getGroupVisitorBook(id, inputdate)
         console.log(data)
+        for( let i = 0 ; i<data.length ; i++) {
+          this.canvaslist.push(data[i].imgUrl)
+        }
       }
       catch(e) {
         console.log(e)
